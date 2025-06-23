abb865e0ff678fcdeda7e382590b5fa184377d28
sa_rehman <contactsyed135@gmail.com>
2025-06-23 22:28:26 +0530

    Project Structure Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",     # Project name
+    version="0.0.1",                    # Version
+    author="Syed Rehman",              # Your name
+    author_email="contactsyed135@gmail.com",     # Your email
+    packages=find_packages()           # Find all your code automatically
+)
